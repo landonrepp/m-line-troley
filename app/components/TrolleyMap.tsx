e9f3a27a63ab72abe1f1196486442d96e817e1c1
landonrepp <landonrepp@gmail.com>
2021-08-04 09:52:15 -0500

    added user circle to map

--- app/components/TrolleyMap.tsx
+++ app/components/TrolleyMap.tsx
@@ -1,19 +1,61 @@
-import React from "react";
-import { Dimensions } from "react-native";
-import MapView from "react-native-maps";
+import React, { useEffect, useState } from "react";
+import { Dimensions, View, Text } from "react-native";
+import MapView, { Circle, Marker, Region } from "react-native-maps";
+import * as Location from 'expo-location';
 
 
 export default function TrolleyMap(){
+    const [userLocation, setUserLocation] = useState<Location.LocationObject|null>(null);
+    const latDelta = .0372;
+    const longDelta = latDelta;
+    const [userCircleRadius, setUserCircleRadius] = useState(100);
+    const window = Dimensions.get('window');
+    const { width, height }  = window;
+
+    const setUserRadiusByLatDelta = (delta: number)=>setUserCircleRadius(delta * width/height * 3500);
+
+    const onRegionChange = (region: Region)=>{
+        setUserRadiusByLatDelta(region.latitudeDelta);
+    }
+
+    useEffect(() => {
+        (async () => {
+          let { status } = await Location.requestForegroundPermissionsAsync();
+          if (status !== 'granted') {
+            console.log('Permission to access location was denied');
+            return;
+          }
+          let location = await Location.getCurrentPositionAsync({});
+          console.log(`user location set ${JSON.stringify(location.coords)}`);
+          setUserRadiusByLatDelta(latDelta);
+          setUserLocation(location);
+          
+        })();
+      }, []);
+
     return (
         <MapView 
             style={styles.map}
+            onRegionChange={onRegionChange}
             initialRegion={{
                 latitude:32.7834,
                 longitude: -96.7984,
-                latitudeDelta:.0372,
-                longitudeDelta:.0372
-            }} 
-        />
+                latitudeDelta:latDelta,
+                longitudeDelta:longDelta
+            }} >
+            {
+                userLocation == null?
+                    null:
+                    <Circle 
+                        radius={userCircleRadius}
+                        fillColor={'lightblue'}
+                        center={{
+                            latitude:userLocation.coords.latitude,
+                            longitude: userLocation.coords.longitude
+                        }}
+                    />
+            }
+        </MapView>
     );
 }
 
@@ -23,4 +65,7 @@ const styles = {
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height
     },
+    container:{
+        
+    }
 }
