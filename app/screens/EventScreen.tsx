99f739cb0f66517014dacddc1918307326a10531
landonrepp <landonrepp@gmail.com>
2021-09-09 19:38:21 -0500

    moved prettier file. cleaned up markup

--- app/screens/EventScreen.tsx
+++ app/screens/EventScreen.tsx
@@ -1,28 +1,48 @@
+import { NavigationProp, useNavigation } from '@react-navigation/core';
+import { openURL } from 'expo-linking';
 import * as React from 'react';
+import { useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { DrawerParamList } from '../types';
 
 export default function EventScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-    </View>
-  );
+	const navigation = useNavigation();
+	const navigationRef = useNavigation<NavigationProp<DrawerParamList>>();
+
+	React.useEffect(() => {
+		const unsubscribe = navigation.addListener('focus', () => {
+			navigationRef.navigate('Tracker');
+			openURL('http://www.mata.org/about/news-events/');
+		});
+
+		return unsubscribe;
+	}, [navigation, navigationRef]);
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Tab Two</Text>
+			<View
+				style={styles.separator}
+				lightColor="#eee"
+				darkColor="rgba(255,255,255,0.1)"
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: '80%',
+	},
 });
